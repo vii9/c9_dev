cded8bf3db7b34edaf826b8d1cfa4192a0096518
ag.thanhz <ag.thanhz@gmail.com>
2022-08-08 22:51:25 +0700

    add js package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+import { formatDistance, subDays } from 'date-fns'
+import {greeting} from "./functions";
+
+let checkDate = formatDistance(subDays(new Date(), 5), new Date(), { addSuffix: true })
+alert(greeting(checkDate));
